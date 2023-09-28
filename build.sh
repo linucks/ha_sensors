96667535263b791a8b7cca54e98c5c601a37b2d5
Jens Thomas <linucks42@gmail.com>
2023-09-28 12:16:15 +0100

    Updates

--- build.sh
+++ build.sh
@@ -4,13 +4,15 @@ IMAGE_NAME=rpisensors
 IMAGE_TAG=latest
 SENSOR_DIR=/opt/fu_sensors
 
-export CR_PAT=ghp_WIs2KeqQDmn1jmhtlKp17R6PQ29UXHYiOr2aECW7
+export CR_PAT=ghp_WIs2jjmhtKeqQDmn1lKp17R6PQ29UXHYiOr2aECW7
 echo $CR_PAT | docker login ghcr.io -u linucks --password-stdin
 
 # Docker needs relative paths
-ln -s $SENSOR_DIR/sensors/rpi .
-ln -s $SENSOR_DIR/sensors/rpi_arduino_shield .
+# ln -s $SENSOR_DIR/sensors/rpi .
+# ln -s $SENSOR_DIR/sensors/rpi_arduino_shield .
+cp -r $SENSOR_DIR/sensors/rpi .
+cp -r $SENSOR_DIR/sensors/rpi_arduino_shield .
 
 # Buld and push
-docker build -t $IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG . \ &&
- docker push $IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG  
+docker build -t $IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG . && \
+docker push $IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG  
