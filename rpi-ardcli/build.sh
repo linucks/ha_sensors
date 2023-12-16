3e49250877a7113d4a96ae7fea7a5447133ea596
Jens Thomas <linucks42@gmail.com>
2023-12-16 18:22:24 +0000

    Trying to get upload working

--- rpi-ardcli/build.sh
+++ rpi-ardcli/build.sh
@@ -5,13 +5,13 @@ IMAGE_REPO=ghcr.io/linucks
 IMAGE_NAME=rpi-ardcli
 IMAGE_TAG=latest
 
-export CR_PAT=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
+export CR_PAT=ghp_jmhtPrv0pWqE94n3Bnzr4Vxth9zl4JzDSr0g5lzi
 echo $CR_PAT | docker login ghcr.io --username linucks --password-stdin
 
 # Docker needs relative paths
 cp -r $SENSOR_DIR/sensors/rpi_arduino_shield .
-# Remove ino file so that we mount in a local version
-rm rpi_arduino_shield/rpi_arduino_shield.ino
+# Remove ino file so that we can copy in a local version
+# rm rpi_arduino_shield/rpi_arduino_shield/rpi_arduino_shield.ino
 
 # Buld and push
 #docker tag  $IMAGE_NAME $IMAGE_REPO/$IMAGE_NAME:$IMAGE_TAG 
